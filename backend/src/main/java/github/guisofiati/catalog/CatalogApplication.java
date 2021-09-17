a58ec0fe6e085e5ba31af3016dd8929fa18fb986
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-17 18:27:43 -0300

    Backend project created (Boilerplate)

---
+++ backend/src/main/java/github/guisofiati/catalog/CatalogApplication.java
@@ -0,0 +1,13 @@
+package github.guisofiati.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}

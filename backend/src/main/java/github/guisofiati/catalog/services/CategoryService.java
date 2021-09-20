738894047bc6e0e92ee73cb2a5b09e1ea6b07e43
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-20 15:43:37 -0300

    Delete category

--- backend/src/main/java/github/guisofiati/catalog/services/CategoryService.java
+++ backend/src/main/java/github/guisofiati/catalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import github.guisofiati.catalog.dto.CategoryDTO;
 import github.guisofiati.catalog.entities.Category;
 import github.guisofiati.catalog.repositories.CategoryRepository;
+import github.guisofiati.catalog.services.exceptions.DatabaseException;
 import github.guisofiati.catalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -51,6 +54,16 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id " +id+ " not found");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " +id+ " not found");
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }

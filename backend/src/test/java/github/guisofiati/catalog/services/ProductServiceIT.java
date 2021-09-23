e9fe4c0c4b9ab7f2e78e02adcd0561ba59c4602f
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-23 11:00:11 -0300

    Integration tests: Delete

---
+++ backend/src/test/java/github/guisofiati/catalog/services/ProductServiceIT.java
@@ -0,0 +1,48 @@
+package github.guisofiati.catalog.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import github.guisofiati.catalog.repositories.ProductRepository;
+import github.guisofiati.catalog.services.exceptions.ResourceNotFoundException;
+
+@SpringBootTest
+public class ProductServiceIT {
+	
+	@Autowired
+	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExists() {
+		
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
+	}
+}
